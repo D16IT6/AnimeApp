c5f44c6fdb5d32656e1f7bb6674b94b3ded904d2
LaTienAnh <latienanh328@gmail.com>
2023-12-17 21:40:30 +0700

    hoan thanh navigation

--- src/Screens/animeDetailsScreens/Comments.tsx
+++ src/Screens/animeDetailsScreens/Comments.tsx
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-import React,{useEffect, useState} from "react";
-import { Comments, InputComment, KeyboardAvoidingContainer, NavagitonTop, Reply } from "../../common/component";
-import { KeyboardAvoidingView, SafeAreaView, Text, View,ScrollView, Dimensions, StyleSheet, FlatList } from "react-native";
-=======
 import React, { useEffect, useState } from "react";
 import { Comments, InputComment, NavagitonTop } from "../../common/component";
 import { SafeAreaView, View, Dimensions, StyleSheet, FlatList } from "react-native";
->>>>>>> 07e9af72bb4896cb05438ea3406b91b9a6850cde
 import { useNavigation } from "@react-navigation/native";
 import { AuthRoutes, AuthScreenNavigationProps } from "../../navigations/AuthNavigator";
 import { Color } from "../../common/Colors";
 import { CommentApi } from "../../apiService/CommentService";
-<<<<<<< HEAD
-import {  CommentResponseView } from "../../ModelView";
-import { CommentsRouteProps } from "../../navigations/AuthNavigator/Type";
-
-const {width,height} = Dimensions.get("window")
-const CommentsScreens = ({route}:CommentsRouteProps)=>{
-    const{animeId}=route.params;
-    const navigation = useNavigation<AuthScreenNavigationProps>()
-    const[backEndComments,setBackEndComments] = useState<CommentResponseView[]>();
-=======
 import { CommentResponseView } from "../../ModelView";
 import { CommentsRouteProps } from "../../navigations/AuthNavigator/Type";
 
@@ -29,7 +13,6 @@ const CommentsScreens = ({ route }: CommentsRouteProps) => {
     const { animeId } = route.params;
     const navigation = useNavigation<AuthScreenNavigationProps>()
     const [backEndComments, setBackEndComments] = useState<CommentResponseView[]>();
->>>>>>> 07e9af72bb4896cb05438ea3406b91b9a6850cde
 
     // const rootComments = backEndComments.filter((Comments)=>{
     //     return Comments.parentId===null
@@ -39,39 +22,6 @@ const CommentsScreens = ({ route }: CommentsRouteProps) => {
     //     .filter((backEndComments)=> backEndComments.parentId===commentId)
     //     .sort((a,b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
     // }
-<<<<<<< HEAD
-    useEffect (()=>{
-                const fetchData = async ()=>{
-            
-                const resultAllComment= await CommentApi.getAllComment(animeId)
-                setBackEndComments(x=>resultAllComment)
-                }
-                fetchData()
-            },[])
-    return (
-       
-        <SafeAreaView style={{flex:1,backgroundColor:Color.SecondaryColor}}>
-                <NavagitonTop
-        title={` Comments`}
-        OnPressArrowBack={()=>{
-            navigation.navigate(AuthRoutes.AnimeDetails,{animeId:animeId})
-        }}
-        group={true}
-        ></NavagitonTop>
-        <FlatList  style={styles.scrollViewContainer}            
-                        data={backEndComments}
-                        keyExtractor={(item) => item.Id.toString()}
-                        renderItem={({ item }: { item: CommentResponseView, index: number }) => {
-                            return (
-                                <Comments 
-                                key={item.Id}
-                                comment ={item}
-                                /> 
-                            )
-                        }}
-                    />
-        {/* <ScrollView style={styles.scrollViewContainer}>
-=======
     useEffect(() => {
         const fetchData = async () => {
 
@@ -103,7 +53,6 @@ const CommentsScreens = ({ route }: CommentsRouteProps) => {
                 }}
             />
             {/* <ScrollView style={styles.scrollViewContainer}>
->>>>>>> 07e9af72bb4896cb05438ea3406b91b9a6850cde
              {rootComments.map((rootComments)=>{
                 return<Comments 
                 key={rootComments.id}
@@ -112,19 +61,11 @@ const CommentsScreens = ({ route }: CommentsRouteProps) => {
                 /> 
              })}
         </ScrollView> */}
-<<<<<<< HEAD
-        <View style={styles.bottomInput}>    
-            <InputComment 
-            animeId={animeId}
-            ></InputComment>
-        </View>
-=======
             <View style={styles.bottomInput}>
                 <InputComment
                     animeId={animeId}
                 ></InputComment>
             </View>
->>>>>>> 07e9af72bb4896cb05438ea3406b91b9a6850cde
         </SafeAreaView>
     )
 }
--- src/navigations/AuthNavigator/AuthNavigator.tsx
+++ src/navigations/AuthNavigator/AuthNavigator.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { WelcomeScreen, LoginScreen, SignUpScreen, LoginMethodScreen, ResetWelcome } from '../../Screens/AuthScreens'
 import { MainNavigationBar, HitAnime, Notification, Filter, SearchAnime, NewEpisodeReleases } from '../../Screens/homeScreen'
@@ -7,47 +7,55 @@ import { AuthRoutes } from './Routes'
 import { getItem, setItem } from '../../utils/asyncStorage'
 import { AnimeDetails, CommentsScreens, VideoPlayScreen } from '../../Screens/animeDetailsScreens'
 const Stack = createNativeStackNavigator<AuthScreenNavigationProps>();
-const getAccessToken = async (): Promise<string | undefined> => {
-  return getItem("AccessToken")
-    .then((accessToken) => accessToken?.toString())
-    .catch((error) => {
-      console.error("Error getting access token:", error);
-      return undefined;
-    });
-};
-let firstRoute = AuthRoutes.MainNavigationBar;
-(async () => {
-  try {
-    const token = await getAccessToken();
-    const isFirstWelcome = (await getItem('welcomed')) == '1';
 
-    console.log('token: ' + token)
-    console.log('isFirstWelcome: ' + isFirstWelcome)
-    if (isFirstWelcome) {
-      await setItem('welcomed', '0')
-      firstRoute = AuthRoutes.Welcome
-      return;
-    }
-    if (token === undefined) {
-      console.log('da toi day')
-      firstRoute = AuthRoutes.LoginMethod
-    }
-    else {
-      firstRoute = AuthRoutes.MainNavigationBar;
-
-    }
-  } catch (error) {
-    console.error("Error during app initialization:", error);
-  }
-})();
 const AuthNavigator = () => {
+  const getAccessToken = async (): Promise<string | undefined | null> => {
+    return await getItem("AccessToken");
+  };
+  const [firstRoute, setFirstRoute] = useState<string | undefined>();
+  const [showWelcome, setWelcome] = useState<boolean | null>(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useLayoutEffect(() => {
+    const checkIfAlreadyWelcomed = async () => {
+      let welcomed = await getItem('welcomed');
+      console.log(welcomed);
+      const token = await getAccessToken();
+      console.log(token)
+      if (welcomed === '1') {
+        setWelcome(false);
+        if(token===null||token===undefined){
+          console.log("vào login")
+          setFirstRoute(AuthRoutes.Login);
+        }
+        else{
+          console.log("vào main")
+          setFirstRoute(AuthRoutes.MainNavigationBar);
+        }
+      } else {
+        setWelcome(true);
+        setFirstRoute(AuthRoutes.Welcome);
+      }
+
+      setLoading(false); // Đánh dấu tác vụ đã hoàn thành
+    };
+
+    checkIfAlreadyWelcomed();
+  }, []); 
+
+  // Các useEffect khác
+
+  if (isLoading) {
+    return null; // Hoặc bạn có thể hiển thị một Spinner component trong quá trình loading
+  }
+  console.log("man hinh",firstRoute)
   return (
     <Stack.Navigator initialRouteName={firstRoute}>
-      <Stack.Screen name={AuthRoutes.Welcome} component={WelcomeScreen} options={{ headerShown: false }}></Stack.Screen>
+      <Stack.Screen name={AuthRoutes.Welcome} component={WelcomeScreen} options={{ headerShown: false }} key={AuthRoutes.Welcome}></Stack.Screen>
       <Stack.Screen name={AuthRoutes.LoginMethod} component={LoginMethodScreen} options={{ headerShown: false }}></Stack.Screen>
-      <Stack.Screen name={AuthRoutes.Login} component={LoginScreen} options={{ headerShown: false }}></Stack.Screen>
+      <Stack.Screen name={AuthRoutes.Login} component={LoginScreen} options={{ headerShown: false }} key={AuthRoutes.Login}></Stack.Screen>
       <Stack.Screen name={AuthRoutes.SignUp} component={SignUpScreen} options={{ headerShown: false }}></Stack.Screen>
-      <Stack.Screen name={AuthRoutes.MainNavigationBar} component={MainNavigationBar} options={{ headerShown: false }}></Stack.Screen>
+      <Stack.Screen name={AuthRoutes.MainNavigationBar} component={MainNavigationBar} options={{ headerShown: false }} key={AuthRoutes.MainNavigationBar}></Stack.Screen>
       <Stack.Screen name={AuthRoutes.HitAnime} component={HitAnime} options={{ headerShown: false }}></Stack.Screen>
       <Stack.Screen name={AuthRoutes.NewEpisodeReleases} component={NewEpisodeReleases} options={{ headerShown: false }}></Stack.Screen>
       <Stack.Screen name={AuthRoutes.Notification} component={Notification} options={{ headerShown: false }}></Stack.Screen>
